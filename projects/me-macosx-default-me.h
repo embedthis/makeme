271702b564a1218af24601f31e4058e6ca74fa68
Embedthis Software <dev@embedthis.com>
2014-11-07 13:35:42 -0800

    UPGRADE: paks

--- projects/me-macosx-default-me.h
+++ projects/me-macosx-default-me.h
@@ -226,7 +226,7 @@
 
 /* Profile */
 #ifndef ME_CONFIG_CMD
-    #define ME_CONFIG_CMD "me -d -q -platform macosx-x64-default -configure . -with est -gen make"
+    #define ME_CONFIG_CMD "me -d -q -platform macosx-x64-default -configure . --with est -gen xcode"
 #endif
 #ifndef ME_ME_PRODUCT
     #define ME_ME_PRODUCT 1
