92e2aa95486f1678cb41b4d699706cd3b53daf38
Embedthis Software <dev@embedthis.com>
2015-04-27 10:49:53 -0700

    REFACTOR: bump version

--- projects/me-windows-default-me.h
+++ projects/me-windows-default-me.h
@@ -136,7 +136,7 @@
     #define ME_TUNE "speed"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "0.8.7"
+    #define ME_VERSION "0.8.8"
 #endif
 
 /* Prefixes */
@@ -234,10 +234,10 @@
     #define ME_MINOR_VERSION 8
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 7
+    #define ME_PATCH_VERSION 8
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 07
+    #define ME_VNUM 08
 #endif
 
 /* Components */
