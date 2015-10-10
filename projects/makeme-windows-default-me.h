a67e9871066acce0926118465b0bdd7181dd80ba
Embedthis Software <dev@embedthis.com>
2015-10-09 19:56:48 -0700

    REGEN: doc and projects

--- projects/makeme-windows-default-me.h
+++ projects/makeme-windows-default-me.h
@@ -127,7 +127,7 @@
     #define ME_TUNE "speed"
 #endif
 #ifndef ME_VERSION
-    #define ME_VERSION "0.10.0"
+    #define ME_VERSION "0.10.1"
 #endif
 
 /* Prefixes */
@@ -225,10 +225,10 @@
     #define ME_MINOR_VERSION 10
 #endif
 #ifndef ME_PATCH_VERSION
-    #define ME_PATCH_VERSION 0
+    #define ME_PATCH_VERSION 1
 #endif
 #ifndef ME_VNUM
-    #define ME_VNUM 80000
+    #define ME_VNUM 80001
 #endif
 
 /* Components */
